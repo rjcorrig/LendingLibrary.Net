d80666a40cddc1dba6eca97373ac66d5c2f6ca68
Robert Corrigan <rjcorrig@gmail.com>
2017-12-10 19:21:39 -0500

    Add tests for BooksController

--- LendingLibrary.Tests/Controllers/BooksControllerTests.cs
+++ LendingLibrary.Tests/Controllers/BooksControllerTests.cs
@@ -21,6 +21,8 @@ using System;
 using System.Threading.Tasks;
 using System.Web.Mvc;
 using LendingLibrary.Controllers;
+using LendingLibrary.Tests.Models;
+using LendingLibrary.Models;
 
 namespace LendingLibrary.Tests.Controllers
 {
@@ -28,12 +30,27 @@ namespace LendingLibrary.Tests.Controllers
     public class BooksControllerTests
     {
         [Test()]
-        public async Task TestDetailsView()
+        public async Task BooksController_Details_returns_ViewResult_if_found()
         {
-            // TODO: Fix
-            var controller = new BooksController();
-            var result = await controller.Details(32) as ViewResult;
-            Assert.AreEqual("Details", result.ViewName);
+            var mockContext = new MockContext();
+            var controller = new BooksController(mockContext.Object);
+            var result = await controller.Details(43) as ViewResult;
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOf(typeof(Book), result.Model);
+
+            var model = result.Model as Book;
+            Assert.AreEqual(model.ID, 43);
         }
+
+        [Test()]
+        public async Task BooksController_Details_returns_HttpNotFoundResult_if_not_found()
+        {
+            var mockContext = new MockContext();
+            var controller = new BooksController(mockContext.Object);
+            var result = await controller.Details(41) as HttpNotFoundResult;
+
+            Assert.IsNotNull(result);
+        }    
     }
 }
