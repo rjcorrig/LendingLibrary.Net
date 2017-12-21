3871a45fc3a5a095eafc0e2d42cddc317b4ef182
Robert Corrigan <rjcorrig@gmail.com>
2017-12-20 21:34:21 -0500

    Add ErrorController and NotFound page

---
+++ LendingLibrary/Controllers/ErrorController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LendingLibrary.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View("Error");
+        }
+
+        public ActionResult NotFound()
+        {
+            return View("NotFound");
+        }
+    }
+}
