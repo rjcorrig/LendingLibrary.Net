498a3159a5d6e15b8229bad14218680aac9f73be
Robert Corrigan <rjcorrig@gmail.com>
2017-12-22 23:11:19 -0500

    Add Forbidden ErrorController test

--- LendingLibrary.Tests/Controllers/ErrorControllerTests.cs
+++ LendingLibrary.Tests/Controllers/ErrorControllerTests.cs
@@ -5,6 +5,7 @@ using System.Web.Mvc;
 using Moq;
 using System.Web;
 using System.Web.Routing;
+using System.Net;
 
 namespace LendingLibrary.Tests.Controllers
 {
@@ -12,7 +13,7 @@ namespace LendingLibrary.Tests.Controllers
     public class ErrorControllerTests
     {
         [Test()]
-        public void Index_returns_Error_View()
+        public void Index_returns_Error_View_with_500()
         {
             var response = new Mock<HttpResponseBase>();
             response.SetupProperty(r => r.StatusCode);
@@ -20,7 +21,11 @@ namespace LendingLibrary.Tests.Controllers
             var context = new Mock<HttpContextBase>();
             context.Setup(c => c.Response).Returns(response.Object);
 
-            var reqCtx = new RequestContext(context.Object, new RouteData());
+            var routeData = new RouteData();
+            routeData.Values["controller"] = "AController";
+            routeData.Values["action"] = "AnAction";
+
+            var reqCtx = new RequestContext(context.Object, routeData);
 
 			var controller = new ErrorController();
 
@@ -31,7 +36,34 @@ namespace LendingLibrary.Tests.Controllers
 
             Assert.IsInstanceOf(typeof(ViewResult), result);
             Assert.AreEqual("Error", ((ViewResult)result).ViewName);
-            Assert.AreEqual(500, response.Object.StatusCode);
+            Assert.AreEqual((int)HttpStatusCode.InternalServerError, response.Object.StatusCode);
+        }
+
+        [Test()]
+        public void Forbidden_returns_Forbidden_View_with_403()
+        {
+            var response = new Mock<HttpResponseBase>();
+            response.SetupProperty(r => r.StatusCode);
+
+            var context = new Mock<HttpContextBase>();
+            context.Setup(c => c.Response).Returns(response.Object);
+
+            var routeData = new RouteData();
+            routeData.Values["controller"] = "AController";
+            routeData.Values["action"] = "AnAction";
+
+            var reqCtx = new RequestContext(context.Object, routeData);
+
+            var controller = new ErrorController();
+
+            var ctlCtx = new ControllerContext(reqCtx, controller);
+            controller.ControllerContext = ctlCtx;
+
+            var result = controller.Forbidden();
+
+            Assert.IsInstanceOf(typeof(ViewResult), result);
+            Assert.AreEqual("Forbidden", ((ViewResult)result).ViewName);
+            Assert.AreEqual((int)HttpStatusCode.Forbidden, response.Object.StatusCode);
         }
     }
 }
