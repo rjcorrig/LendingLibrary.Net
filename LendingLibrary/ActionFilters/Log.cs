61a07783021b59a4ed36278365d413629a834c83
Robert Corrigan <rjcorrig@gmail.com>
2017-12-23 16:19:50 -0500

    Add more status logging to Log action filter

--- LendingLibrary/ActionFilters/Log.cs
+++ LendingLibrary/ActionFilters/Log.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
 
@@ -9,29 +10,30 @@ namespace LendingLibrary.ActionFilters
     {
         public override void OnActionExecuting(ActionExecutingContext filterContext)
         {
-            LogEvent("OnActionExecuting", filterContext.RouteData, filterContext.HttpContext.Response.StatusCode);
+            LogEvent("OnActionExecuting", filterContext.RouteData, filterContext.HttpContext.Response);
         }
 
         public override void OnActionExecuted(ActionExecutedContext filterContext)
         {
-            LogEvent("OnActionExecuted", filterContext.RouteData, filterContext.HttpContext.Response.StatusCode);
+            LogEvent("OnActionExecuted", filterContext.RouteData, filterContext.HttpContext.Response);
         }
 
         public override void OnResultExecuting(ResultExecutingContext filterContext)
         {
-            LogEvent("OnResultExecuting", filterContext.RouteData, filterContext.HttpContext.Response.StatusCode);
+            LogEvent("OnResultExecuting", filterContext.RouteData, filterContext.HttpContext.Response);
         }
 
         public override void OnResultExecuted(ResultExecutedContext filterContext)
         {
-            LogEvent("OnResultExecuted", filterContext.RouteData, filterContext.HttpContext.Response.StatusCode);
+            LogEvent("OnResultExecuted", filterContext.RouteData, filterContext.HttpContext.Response);
         }
 
-        private void LogEvent(string methodName, RouteData routeData, int status)
+        private void LogEvent(string methodName, RouteData routeData, HttpResponseBase response)
         {
             var controllerName = routeData.Values["controller"];
             var actionName = routeData.Values["action"];
-            var message = String.Format("{0} controller:{1} action:{2} status:{3}", methodName, controllerName, actionName, status);
+            var message = $@"{methodName} controller:{controllerName} action:{actionName}
+            status:{response.Status} code:{response.StatusCode} sub:{response.SubStatusCode} desc:{response.StatusDescription}";
             Debug.WriteLine(message, "Action Filter Log");
         }
     }
