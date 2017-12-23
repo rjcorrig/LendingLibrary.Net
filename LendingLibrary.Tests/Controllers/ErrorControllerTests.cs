f7085a41ae391e4a7c649c0246684e77e123f969
Robert Corrigan <rjcorrig@gmail.com>
2017-12-22 23:30:38 -0500

    Add more ErrorController tests

--- LendingLibrary.Tests/Controllers/ErrorControllerTests.cs
+++ LendingLibrary.Tests/Controllers/ErrorControllerTests.cs
@@ -12,10 +12,13 @@ namespace LendingLibrary.Tests.Controllers
     [TestFixture()]
     public class ErrorControllerTests
     {
-        [Test()]
-        public void Index_returns_Error_View_with_500()
+        protected ErrorController controller;
+        protected Mock<HttpResponseBase> response;
+
+        [SetUp()]
+        public void SetupController()
         {
-            var response = new Mock<HttpResponseBase>();
+            response = new Mock<HttpResponseBase>();
             response.SetupProperty(r => r.StatusCode);
 
             var context = new Mock<HttpContextBase>();
@@ -27,43 +30,73 @@ namespace LendingLibrary.Tests.Controllers
 
             var reqCtx = new RequestContext(context.Object, routeData);
 
-			var controller = new ErrorController();
+            controller = new ErrorController();
 
             var ctlCtx = new ControllerContext(reqCtx, controller);
             controller.ControllerContext = ctlCtx;
+        }
 
+        [Test()]
+        public void Index_returns_Error_View_with_500()
+        {
             var result = controller.Index();
 
             Assert.IsInstanceOf(typeof(ViewResult), result);
-            Assert.AreEqual("Error", ((ViewResult)result).ViewName);
+
+            var viewResult = result as ViewResult;
+            Assert.AreEqual("Error", viewResult.ViewName);
+
             Assert.AreEqual((int)HttpStatusCode.InternalServerError, response.Object.StatusCode);
         }
 
         [Test()]
         public void Forbidden_returns_Forbidden_View_with_403()
         {
-            var response = new Mock<HttpResponseBase>();
-            response.SetupProperty(r => r.StatusCode);
-
-            var context = new Mock<HttpContextBase>();
-            context.Setup(c => c.Response).Returns(response.Object);
-
-            var routeData = new RouteData();
-            routeData.Values["controller"] = "AController";
-            routeData.Values["action"] = "AnAction";
-
-            var reqCtx = new RequestContext(context.Object, routeData);
-
-            var controller = new ErrorController();
-
-            var ctlCtx = new ControllerContext(reqCtx, controller);
-            controller.ControllerContext = ctlCtx;
-
             var result = controller.Forbidden();
 
             Assert.IsInstanceOf(typeof(ViewResult), result);
-            Assert.AreEqual("Forbidden", ((ViewResult)result).ViewName);
+
+            var viewResult = result as ViewResult;
+            Assert.AreEqual("Forbidden", viewResult.ViewName);
+
             Assert.AreEqual((int)HttpStatusCode.Forbidden, response.Object.StatusCode);
+
+            Assert.IsInstanceOf(typeof(HandleErrorInfo), viewResult.Model);
+            Assert.AreEqual("Forbidden", (viewResult.Model as HandleErrorInfo)?.Exception?.Message);
         }
+
+        [Test()]
+        public void Forbidden_returns_Forbidden_View_with_403_and_message()
+        {
+            var message = "A message";
+            controller.TempData["Message"] = message;
+            var result = controller.Forbidden();
+
+            Assert.IsInstanceOf(typeof(ViewResult), result);
+
+            var viewResult = result as ViewResult;
+            Assert.AreEqual("Forbidden", viewResult.ViewName);
+
+            Assert.AreEqual((int)HttpStatusCode.Forbidden, response.Object.StatusCode);
+
+            Assert.IsInstanceOf(typeof(HandleErrorInfo), viewResult.Model);
+            Assert.AreEqual(message, (viewResult.Model as HandleErrorInfo)?.Exception?.Message);
+        }
+
+        [Test()]
+        public void Forbidden_returns_Forbidden_View_with_custom_httpStatus()
+        {
+            var status = (int)HttpStatusCode.NotImplemented;
+            controller.TempData["StatusCode"] = status;
+            var result = controller.Forbidden();
+
+            Assert.IsInstanceOf(typeof(ViewResult), result);
+
+            var viewResult = result as ViewResult;
+            Assert.AreEqual("Forbidden", viewResult.ViewName);
+
+            Assert.AreEqual(status, response.Object.StatusCode);
+        }
+
     }
 }
