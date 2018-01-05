b7a3277a0f02f77114f2a3993961010068d652fe
Robert Corrigan <rjcorrig@gmail.com>
2018-01-04 21:14:35 -0500

    Add testing constructor for FriendshipsController

---
+++ LendingLibrary.Tests/Controllers/FriendshipsControllerTests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+using System;
+namespace LendingLibrary.Tests.Controllers
+{
+    [TestFixture()]
+    public class FriendshipsControllerTests
+    {
+        [Test()]
+        public void TestCase()
+        {
+        }
+    }
+}
