b776e51b5f02704fdbe6002ca135975a632e1b86
Robert Corrigan <rjcorrig@gmail.com>
2018-01-05 06:33:05 -0500

    Add Index and Waiting tests for FriendshipsController

--- LendingLibrary.Tests/Controllers/FriendshipsControllerTests.cs
+++ LendingLibrary.Tests/Controllers/FriendshipsControllerTests.cs
@@ -13,7 +13,7 @@ namespace LendingLibrary.Tests.Controllers
     public class FriendshipsControllerTests
     {
         [Test()]
-        public async Task Index_returns_logged_in_Users_Friendships()
+        public async Task Index_returns_logged_in_Users_Friendships_in_View()
         {
             var userId = "foxyboots9-guid";
             var mockDbContext = new MockContext();
@@ -30,5 +30,55 @@ namespace LendingLibrary.Tests.Controllers
                 Assert.That(friendship.FriendId == userId || friendship.UserId == userId);
             }
         }    
+
+        [Test()]
+        [TestCase(false)]
+        [TestCase(true)]
+        public async Task Index_passes_RequestSent_to_ViewBag(bool RequestSent)
+        {
+            var userId = "foxyboots9-guid";
+            var mockDbContext = new MockContext();
+            var controller = new FriendshipsController(mockDbContext.Object, () => userId);
+
+            var result = await controller.Index(RequestSent) as ViewResult;
+            Assert.IsNotNull(result);
+
+            Assert.AreEqual(RequestSent, result.ViewBag.RequestSent);
+        }    
+
+        [Test()]
+        public async Task Waiting_returns_Friendships_waiting_for_approval()
+        {
+            var userId = "foxyboots9-guid";
+            var mockDbContext = new MockContext();
+            var controller = new FriendshipsController(mockDbContext.Object, () => userId);
+
+            var result = await controller.Waiting() as ViewResult;
+            Assert.IsNotNull(result);
+
+            var model = result.Model as IEnumerable<Friendship>;
+            Assert.IsNotNull(model);
+
+            foreach (var friendship in model)
+            {
+                Assert.That(friendship.FriendId == userId && !friendship.RequestApproved.HasValue);
+            }
+        }    
+
+        [Test()]
+        [TestCase(false)]
+        [TestCase(true)]
+        public async Task Waiting_passes_RequestConfirmed_to_ViewBag(bool RequestConfirmed)
+        {
+            var userId = "foxyboots9-guid";
+            var mockDbContext = new MockContext();
+            var controller = new FriendshipsController(mockDbContext.Object, () => userId);
+
+            var result = await controller.Waiting(RequestConfirmed) as ViewResult;
+            Assert.IsNotNull(result);
+
+            Assert.AreEqual(RequestConfirmed, result.ViewBag.RequestConfirmed);
+        }    
+
     }
 }
