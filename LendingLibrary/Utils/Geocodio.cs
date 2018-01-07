64916b2a978d58b0f2d70aff39a8b317fe88d218
Robert Corrigan <rjcorrig@gmail.com>
2018-01-07 11:08:50 -0500

    Rough in Geocoding call

---
+++ LendingLibrary/Utils/Geocodio.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Data.Entity.Spatial;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using Newtonsoft.Json.Linq;
+using static System.Web.Configuration.WebConfigurationManager;
+
+namespace LendingLibrary.Utils
+{
+    public class Geocodio : IGeocoder
+    {
+        protected readonly string apiKey;
+        protected readonly Uri apiUrl;
+
+        public Geocodio() : this(AppSettings["geocodio-url"], AppSettings["geocodio-apikey"])
+        {
+        }
+
+        public Geocodio(string apiUrl, string apiKey)
+        {
+            this.apiKey = apiKey;
+            this.apiUrl = new Uri(apiUrl);
+        }
+
+        public async Task<DbGeography> GeocodeAsync(string address)
+        {
+            DbGeography result = default(DbGeography);
+
+            using (var httpClient = new HttpClient())
+            {
+                var builder = new UriBuilder(apiUrl);
+                builder.Port = -1;
+                var query = HttpUtility.ParseQueryString(builder.Query);
+                query["q"] = address;
+                query["api_key"] = apiKey;
+                builder.Query = query.ToString();
+
+                using (var httpRequest = new HttpRequestMessage(HttpMethod.Get, builder.ToString()))
+                {
+                    httpRequest.Headers.Add("Accept", "application/json");
+                    using (var httpResponse = await httpClient.SendAsync(httpRequest)) 
+                    {
+                        if (httpResponse.IsSuccessStatusCode)
+                        {
+                            var body = await httpResponse.Content.ReadAsStringAsync();
+                            var response = JObject.Parse(body);
+
+                            var location = response["results"]?[0]?["location"];
+                            var lat = location?["lng"]?.Value<double>() ?? 0;
+                            var lng = location?["lng"]?.Value<double>() ?? 0;
+
+                            return DbGeography.FromText($"POINT ({lat} {lng}");
+                        }
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ LendingLibrary/Utils/IGeocoder.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Data.Entity.Spatial;
+using System.Threading.Tasks;
+
+namespace LendingLibrary.Utils
+{
+    public interface IGeocoder
+    {
+        Task<DbGeography> GeocodeAsync(string address);
+    }
+}
