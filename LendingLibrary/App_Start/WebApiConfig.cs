b666252b2d0ff11101113cc6ef78e630cd85419b
Robert Corrigan <rjcorrig@gmail.com>
2018-04-04 21:43:34 -0400

    Add DI to WebApi per MS docs

--- LendingLibrary/App_Start/WebApiConfig.cs
+++ LendingLibrary/App_Start/WebApiConfig.cs
@@ -8,14 +8,28 @@ namespace LendingLibrary
         {
             // Web API configuration and services
 
+            // Serialize to JSON
+            var json = config.Formatters.JsonFormatter;
+            json.SerializerSettings.PreserveReferencesHandling = Newtonsoft.Json.PreserveReferencesHandling.Objects;
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+
             // Web API routes
             config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "Books",
+                routeTemplate: "api/books/{id}",
+                defaults: new { controller = "BooksApi", id = RouteParameter.Optional }
+            );
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            var container = UnityConfig.Container;
+            config.DependencyResolver = new UnityResolver(container);
         }
     }
 }
---
+++ LendingLibrary/Controllers/BooksApiController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Web.Http;
+using LendingLibrary.Models;
+using Microsoft.AspNet.Identity;
+using Unity.Attributes;
+
+namespace LendingLibrary.Controllers
+{
+    public class BooksApiController : ApiController
+    {
+        protected IApplicationDbContext db;
+        protected IRepository repo;
+        protected Func<string> GetUserId;
+
+        [InjectionConstructor]
+        public BooksApiController(IApplicationDbContext db)
+        {
+            repo = new Repository(db);
+            this.db = repo.Db;
+        }
+
+        public BooksApiController(IApplicationDbContext db, Func<string> getUserId)
+        {
+            repo = new Repository(db);
+            this.db = repo.Db;
+
+            GetUserId = getUserId;
+        }
+
+        protected async Task<ApplicationUser> GetCurrentUserAsync()
+        {
+            if (GetUserId != null)
+            {
+                return await repo.GetUserByIdAsync(GetUserId());
+            }
+            else
+            {
+                return await repo.GetUserByIdAsync(User.Identity.GetUserId());
+            }
+        }
+
+        protected ApplicationUser GetCurrentUser()
+        {
+            if (GetUserId != null)
+            {
+                return repo.GetUserById(GetUserId());
+            }
+            else
+            {
+                return repo.GetUserById(User.Identity.GetUserId());
+            }
+        }
+
+        protected string GetCurrentUserId()
+        {
+            if (GetUserId != null)
+            {
+                return GetUserId();
+            }
+            else
+            {
+                return User.Identity.GetUserId();
+            }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        public async Task<IHttpActionResult> GetBooks()
+        {
+            var currentUserId = GetCurrentUserId();
+            var books = await repo.GetBooksByOwnerIdAsync(currentUserId);
+
+            return Ok(books);
+        }
+
+        public async Task<IHttpActionResult> GetBook(int id)
+        {
+            var book = await repo.GetBookByIdAsync(id);
+            return Ok(book);
+        }
+    }
+}
