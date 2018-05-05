c8a389622d8373a7dfb069c7a8828f50db671f60
Robert Corrigan <rjcorrig@gmail.com>
2018-05-05 17:59:25 -0400

    Add basic Book API xml doc comments

--- LendingLibrary/App_Start/SwaggerConfig.cs
+++ LendingLibrary/App_Start/SwaggerConfig.cs
@@ -2,6 +2,7 @@ using System.Web.Http;
 using WebActivatorEx;
 using LendingLibrary;
 using Swashbuckle.Application;
+using System;
 
 [assembly: PreApplicationStartMethod(typeof(SwaggerConfig), "Register")]
 
@@ -101,7 +102,7 @@ namespace LendingLibrary
                         // those comments into the generated docs and UI. You can enable this by providing the path to one or
                         // more Xml comment files.
                         //
-                        //c.IncludeXmlComments(GetXmlCommentsPath());
+                        c.IncludeXmlComments(GetXmlCommentsPath());
 
                         // Swashbuckle makes a best attempt at generating Swagger compliant JSON schemas for the various types
                         // exposed in your API. However, there may be occasions when more control of the output is needed.
@@ -250,6 +251,11 @@ namespace LendingLibrary
                         //
                         //c.EnableApiKeySupport("apiKey", "header");
                     });
+        }
+
+        private static string GetXmlCommentsPath()
+        {
+            return String.Format(@"{0}\App_Data\XmlDocument.xml", AppDomain.CurrentDomain.BaseDirectory);
         }
     }
 }
