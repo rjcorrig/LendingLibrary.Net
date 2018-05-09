5d5bb7c5c204e8c28376736f318c5c031cd8d47b
Robert Corrigan <rjcorrig@gmail.com>
2018-05-08 22:38:20 -0400

    Swagger: Refactor AddSchemaExamples for readability

--- LendingLibrary/App_Start/AddSchemaExamples.cs
+++ LendingLibrary/App_Start/AddSchemaExamples.cs
@@ -6,34 +6,31 @@ namespace LendingLibrary
 {
     public class AddSchemaExamples : ISchemaFilter
     {
-        private BookDTO book1 = new BookDTO
-        {
-            ID = 1,
-            Author = "Terry Jones",
-            Title = "Who Killed Chaucer?",
-            Rating = 4,
-            Genre = "History",
-            ISBN = "0312335873",
-            OwnerId = "someuser-guid"
-        };
-
         public void Apply(Schema schema, SchemaRegistry schemaRegistry, Type type)
         {
-            if (type == typeof(BookDTO))
+            switch (type.Name)
             {
-                schema.example = book1;
-            }
-            else if (type == typeof(WrappedUnauthorizedApiError))
-            {
-                schema.example = new WrappedUnauthorizedApiError("You must log in to use this API");
-            }
-            else if (type == typeof(WrappedForbiddenApiError))
-            {
-                schema.example = new WrappedForbiddenApiError("You are not allowed to view that resource");
-            }
-            else if (type == typeof(WrappedNotFoundApiError))
-            {
-                schema.example = new WrappedNotFoundApiError("That resource was not found");
+                case nameof(BookDTO):
+                    schema.example = new BookDTO
+                    {
+                        ID = 1,
+                        Author = "Terry Jones",
+                        Title = "Who Killed Chaucer?",
+                        Rating = 4,
+                        Genre = "History",
+                        ISBN = "0312335873",
+                        OwnerId = "someuser-guid"
+                    };
+                    break;
+                case nameof(WrappedUnauthorizedApiError):
+                    schema.example = new WrappedUnauthorizedApiError("You must log in to use this API");
+                    break;
+                case nameof(WrappedForbiddenApiError):
+                    schema.example = new WrappedForbiddenApiError("You are not allowed to view that resource");
+                    break;
+                case nameof(WrappedNotFoundApiError):
+                    schema.example = new WrappedNotFoundApiError("That resource was not found");
+                    break;
             }
         }
     }
