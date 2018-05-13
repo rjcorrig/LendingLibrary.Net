e4773b678f183875079345efb661fac17ea62d55
Robert Corrigan <rjcorrig@gmail.com>
2018-05-13 16:59:58 -0400

    Swagger: Report login failed when no token returns

--- LendingLibrary/SwaggerScripts/onComplete.js
+++ LendingLibrary/SwaggerScripts/onComplete.js
@@ -20,11 +20,15 @@ $.ajax({
     contenttype: 'x-www-form-urlencoded',
     data: "grant_type=password&username=" + credentials[0] + "&password=" + credentials[1],
     success: function (response) {
-        var bearerToken = 'Bearer ' + response.access_token;
-
-        window.swaggerUi.api.clientAuthorizations.add('Authorization', new SwaggerClient.ApiKeyAuthorization('Authorization', bearerToken, 'header'));
-        window.swaggerUi.api.clientAuthorizations.remove("api_key");
-        alert("Login successfull");
+        //alert(JSON.stringify(response));
+        if (response.access_token) {
+            var bearerToken = 'Bearer ' + response.access_token;
+            window.swaggerUi.api.clientAuthorizations.add('Authorization', new SwaggerClient.ApiKeyAuthorization('Authorization', bearerToken, 'header'));
+            window.swaggerUi.api.clientAuthorizations.remove("api_key");
+            alert("Login successful");
+        } else {
+            alert("Login failed!");
+        }
        },
        error: function (xhr, ajaxoptions, thrownerror) {
         alert("Login failed!");
